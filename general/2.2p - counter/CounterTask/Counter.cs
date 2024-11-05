5ad3c714ff3a8f0a84218ec7207e23dcafe1cf74
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2024-11-05 17:43:13 +1100

    oops

---
+++ general/2.2p - counter/CounterTask/Counter.cs
@@ -0,0 +1,37 @@
+﻿namespace CounterTask
+{
+    public class Counter
+    {
+        private long _count; //had to change _count to a long because method ResetByDefault(); puts the value over the 32-bit signed integer limit
+        private string _name;
+        public Counter(string name) {
+            _name = name;
+            _count = 0;
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Reset()
+        {
+        _count = 0; 
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public long Tick
+        {
+        get { return _count; } 
+        }
+
+        public void ResetByDefault()
+        {
+            _count = 2147483647002;
+        }
+    }
+}
---
+++ general/2.2p - counter/CounterTask/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CounterTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter[] myCounters = null;
+            myCounters[2] = new Counter("Counter X");
+        }
+
+        static void PrintCounters(Counter[] counters)
+        {
+            foreach (Counter thisCounter in counters) 
+            
+                Console.WriteLine("{0} is {1}", thisCounter.Name, thisCounter.Tick);
+            }
+    }
+}
